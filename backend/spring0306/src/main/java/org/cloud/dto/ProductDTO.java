3e2f554690533a445a2a7bd0765d3ad90363812c
kimse0ngjun <tjdwns8756@naver.com>
2026-04-13 11:04:24 +0900

    feat: upload only backend projects

---
+++ backend/spring0306/src/main/java/org/cloud/dto/ProductDTO.java
@@ -0,0 +1,18 @@
+package org.cloud.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+
+	private int num;
+	private String name;
+	private int price;
+	private int amount;
+	private String storedFilePath;
+	private MultipartFile file;
+}
